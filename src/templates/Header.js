63ebdc1a2ee0ea7f175c6e1752e70e29a1e9b6d8
WillansMisaelLinux <willansmisaelromerocondori@gmail.com>
2020-04-09 20:28:54 -0400

    Se creo las paginas Home, layout Header and Characters

---
+++ src/templates/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    const view = `
+        <div class="Header-main">
+            <div class="Header-log">
+                <h1>
+                    <a href="/">
+                        100tifi.co
+                </h1>
+            </div>
+            <div class="Header-nav">
+                <a href="#/about/">
+                    About
+                </a>
+            </div>
+        </div>
+    `;
+    return view;
+}
+export default Header;
